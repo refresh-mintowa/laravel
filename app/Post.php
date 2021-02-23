a8c84100091dbd2133e119cdb3851c5f1c5b595a
refresh-mintowa <towagarrix@gmail.com>
2021-02-23 15:48:22 +0900

    ページネーションの追加

--- app/Post.php
+++ app/Post.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-  
+  public function getPaginateByLimit(int $limit_number=10){
+      return $this->orderBy('updated_at','DESC')->paginate($limit_number);
+  }
 }
